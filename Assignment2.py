836ccc73d36317ebe97e5281dde7fdf561cbf413
satyam463 <70562370+satyam463@users.noreply.github.com>
2021-09-12 13:43:22 +0530

    Update Assignment2.py

--- Assignment2.py
+++ Assignment2.py
@@ -1,10 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from numpy import linalg as LA
-      #path to my scripts
+import math
+
 def parab_gen(y,a):
-	x = y**2/a
-	return x
+    x=y**2/a
+    return x
 
 #setting up plot
 fig = plt.figure()
@@ -16,40 +17,54 @@ y = np.linspace(-4,4,len)
 V = np.array(([0,0],[0,1]))
 u = np.array(([-3/2,0]))
 f = -9
-
+#p = np.array(([1,0]))
+#foc = np.abs(p@u)/2
 O = np.array(([0,0]))
 #Eigenvalues and eigenvectors
 D_vec,P = LA.eig(V)
 D = np.diag(D_vec)
+#print(P)
 p = P[:,0]
 eta = 2*u@p
+#foc = np.abs(eta/D_vec[1])
 foc = -eta/D_vec[1]
+print(P[:,0],foc)
 #print(p,foc,D_vec[1])
 x = parab_gen(y,foc)
 #Affine Parameters
 c1 = np.array(([-(u@V@u-2*u@u+f)/(2*u@p),0]))
 c = -P@u+c1
-cA = np.vstack((u+eta*p*0.5,V))
-cb = np.vstack((-f,(0.5*eta*p-u).reshape(-1,1)))
+print(c1)
+#p = -p
+cA = np.vstack((u+eta*0.5*p,V))
+cb = np.vstack((-f,(eta*0.5*p-u).reshape(-1,1)))
 c = LA.lstsq(cA,cb,rcond=None)[0]
 c = c.flatten()
+print(c,foc)
+#print(c,foc)
+#print(cA,cb)
+#print(p,c)
+#c1 = np.array(([(u@V@u-2*D_vec[0]*u@u+D_vec[0]**2*f)/(eta*D_vec[0]**2),0]))
 xStandardparab = np.vstack((x,y))
 #xActualparab = P@(xStandardparab - c1[:,np.newaxis])-u[:,np.newaxis]/D_vec[1]
 xActualparab = P@xStandardparab + c[:,np.newaxis]
+#xActualparab = P@xStandardparab
 
 #Labeling the coordinates
 parab_coords = np.vstack((O,c)).T
 plt.scatter(parab_coords[0,:], parab_coords[1,:])
-vert_labels = ['$O$','$c(-3,0)$']
+vert_labels = ['$O$','$c (-3,0)$']
 for i, txt in enumerate(vert_labels):
     plt.annotate(txt, # this is the text
                  (parab_coords[0,i], parab_coords[1,i]), # this is the point to label
                  textcoords="offset points", # how to position the text
-                 xytext=(0,10), # distance from text to points (x,y)
-                 ha='center') # horizontal alignment can be left
+                 xytext=(0,5), # distance from text to points (x,y)
+                 ha='center') # horizontal alignment can be left, right or center
 
-#Plotting the actual parabola
-plt.plot(xActualparab[0,:],xActualparab[1,:],label='Parabola',color='g')
+
+#Plotting the actual and standard parabola
+plt.plot(xStandardparab[0,:],xStandardparab[1,:],label='Standard Parabola',color='m')
+plt.plot(xActualparab[0,:],xActualparab[1,:],label='Given Parabola',color='g')
 
 plt.xlabel('$x$')
 plt.ylabel('$y$')
